1f772b4afad7cd50033e115afa8210e8d8eb52e6
Unipirate <salamande21@gmail.com>
2025-02-03 15:11:02 +1100

    update

--- Learning/todo/src/app/app.component.ts
+++ Learning/todo/src/app/app.component.ts
@@ -1,21 +1,52 @@
 import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo';
-  placeholder = 'What to do next?';
+  title = 'OneTodo';
+  placeholder = 'What needs to be done????'
 
+  toggleAllBtn = false;
+  check1 = false;
+  check2 = false;
 
-  click() {
-    alert('Button clicked');
+  todoDataList = [
+    {
+      Status: true,
+      Thing: '第一件事情'
+    }, {
+      Status: false,
+      Thing: '第二件事情'
+    }, {
+      Status: false,
+      Thing: '第三件事情'
+    },
+  ];
+
+  toggleAll() {
+    this.toggleAllBtn = !this.toggleAllBtn;
+    this.todoDataList.forEach(data => {
+      data.Status = this.toggleAllBtn;
+    });
   }
 
+  clickCheck(item: any) {
+    item.Status = !item.Status;
+  }
+
+  delete(index: number) {
+    this.todoDataList.splice(index, 1);
+  }
+
+  add(value: string) {
+    this.todoDataList.push({
+      Status: false,
+      Thing: value
+    });
+  }
 
 
 }
---
+++ Learning/todo/src/index.ts
@@ -0,0 +1,4 @@
+export class Todo {
+    Status: boolean;
+    Thing: string;
+}
